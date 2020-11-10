3212f9bfe4b33c988138a7fae023923fa5f862e9
Girish Nivarti <nvgirishs@gmail.com>
2020-11-10 21:21:36 +0000

    Create include files and separate yaml for test

---
+++ loadparams.jl
@@ -0,0 +1,21 @@
+params = YAML.load_file("input.yml"; dicttype=Dict{Symbol,Any})
+
+lx = 4.0*Float64(pi);
+ly = 2.0*Float64(pi);
+nx = params[:nx];
+ny = params[:ny];
+
+Λ = params[:Λ];
+
+Ω = 2.0*Float64(pi);
+θ = params[:θ];
+β = 2.0*Ω*cos(θ);
+Ξ = params[:Ξ]*Ω;
+τ = params[:τ]/Ω;
+Δθ = params[:Δθ];
+
+dt = params[:dt];
+t_end = params[:t_end];
+savefreq = params[:savefreq];
+
+dn = params[:dn];
--- process.jl
+++ process.jl
@@ -1,30 +1,16 @@
-using Revise
 using ZonalFlow
 using Plots
 using YAML,JLD2
 using FFTW
 
-using Logging: global_logger
-using TerminalLoggers: TerminalLogger
-global_logger(TerminalLogger())
-
 include("loadparams.jl")
-
 mkpath(dn);
 
-ζ0 = ic_pert_eqm(lx,ly,nx,ny,Ξ,jw=Δθ); # one ic for all
+@load dn*"nl.jld2" sol1
+@load dn*"gql_$Λ.jld2" sol2
+@load dn*"gce2_$Λ.jld2" sol3
 
-@time sol1 = nl(lx,ly,nx,ny,Ξ,β,τ,jw=Δθ,ic=ζ0,dt=dt,t_end=t_end,savefreq=savefreq);
-@time sol2 = gql(lx,ly,nx,ny,Λ,Ξ,β,τ,jw=Δθ,dt=dt,ic=ζ0,t_end=t_end,savefreq=savefreq);
-@time sol3 = gce2(lx,ly,nx,ny,Λ,Ξ,β,τ,jw=Δθ,ic=ζ0,dt=dt,t_end=t_end,poscheck=false,savefreq=savefreq);
-
-@save dn*"nl.jld2" sol1
-@save dn*"gql_$Λ.jld2" sol2
-@save dn*"gce2_$Λ.jld2" sol3
-
-# pyplot();
-
-## Zonal energy
+pyplot();
 zones = reshape(["$i" for i = 0:1:nx-1],1,nx);
 
 P1,O1 = zonalenergy(lx,ly,nx,ny,sol1.u);
@@ -52,3 +38,21 @@ plot!(angles,A1[end,:],xaxis="θ",yaxis="<ζ>",linewidth=2,label="NL");
 plot!(angles,A2[end,:],xaxis="θ",yaxis="<ζ>",linewidth=2,label="GQL($Λ)");
 _zt = plot!(angles,A3[end,:],legend=:bottomright,xaxis="θ",yaxis="<ζ>",linewidth=2,label="GCE2($Λ)");
 savefig(_zt,dn*"zt_"*"$Λ"*".png");
+
+## Spatial vorticity
+xx = LinRange(-lx/2,lx/2,2*nx-1);
+yy = LinRange(-ly/2,ly/2,2*ny-1);
+
+U1 = inversefourier(nx,ny,sol1.u);
+_u = plot(xx,yy,U1[:,:,begin],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+savefig(_u,dn*"NL_z_init.png");
+_u = plot(xx,yy,U1[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+savefig(_u,dn*"NL_z_end.png");
+
+U2 = inversefourier(nx,ny,sol2.u);
+_u = plot(xx,yy,U2[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+savefig(_u,dn*"GQL_"*"$Λ"*"_z_end.png");
+
+U3 = inversefourier(nx,ny,Λ,sol3.u);
+_u = plot(xx,yy,U3[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+savefig(_u,dn*"GCE2_"*"$Λ"*"_z_end.png");
