97e7273af7430051922f3e3bf89c8f030ae23281
Girish Nivarti <nvgirishs@gmail.com>
2020-11-11 12:29:51 +0000

    Reserve solve for solution and process for results

--- process.jl
+++ process.jl
@@ -1,58 +1,68 @@
 using ZonalFlow
-using Plots
+using Plots: plot,plot!,savefig,pyplot
 using YAML,JLD2
 using FFTW
 
 include("loadparams.jl")
-mkpath(dn);
-
-@load dn*"nl.jld2" sol1
-@load dn*"gql_$Λ.jld2" sol2
-@load dn*"gce2_$Λ.jld2" sol3
 
 pyplot();
+
 zones = reshape(["$i" for i = 0:1:nx-1],1,nx);
 
-P1,O1 = zonalenergy(lx,ly,nx,ny,sol1.u);
-_p = plot(sol1.t,P1,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright,linewidth=2);
+@load dn*"nl.jld2" sol_nl
+@load dn*"ql.jld2" sol_ql
+
+P,O = zonalenergy(lx,ly,nx,ny,sol_nl.u);
+_p = plot(sol_nl.t,P,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright)
 savefig(_p,dn*"NL_em.png");
 
-P2,O2 = zonalenergy(lx,ly,nx,ny,sol2.u);
-_p = plot(sol2.t,P2,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright,linewidth=2);
+P,O = zonalenergy(lx,ly,nx,ny,sol_ql.u);
+_p = plot(sol_ql.t,P,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright)
+savefig(_p,dn*"QL_em.png");
+
+@load dn*"gql_$Λ.jld2" sol_gql
+@load dn*"gce2_$Λ.jld2" sol_gce2
+
+P,O = zonalenergy(lx,ly,nx,ny,sol_gql.u);
+_p = plot(sol_gql.t,P,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright)
 savefig(_p,dn*"GQL_"*"$Λ"*"_em.png");
 
-P3,O3 = zonalenergy(lx,ly,nx,ny,Λ,sol3.u);
-_p = plot(sol3.t,P3,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright,linewidth=2);
+P,O = zonalenergy(lx,ly,nx,ny,Λ,sol_gce2.u);
+_p = plot(sol_gce2.t,P,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright)
 savefig(_p,dn*"GCE2_"*"$Λ"*"_em.png");
 
 ## Hövmoller data: mean vorticity
 angles = (180.0/ly)*LinRange(-ly/2,ly/2,2*ny-1);
-
-A1 = meanvorticity(lx,ly,nx,ny,sol1.u);
-A2 = meanvorticity(lx,ly,nx,ny,sol2.u);
-A3 = meanvorticity(lx,ly,nx,ny,Λ,sol3.u);
-
 Ajet = real(ifft(ifftshift(ic_eqm(lx,ly,nx,ny,Ξ,jw=Δθ)[:,1])));
+
+A_nl = meanvorticity(lx,ly,nx,ny,sol_nl.u);
+A_ql = meanvorticity(lx,ly,nx,ny,sol_ql.u);
+A_gql = meanvorticity(lx,ly,nx,ny,sol_gql.u);
+A_gce2 = meanvorticity(lx,ly,nx,ny,Λ,sol_gce2.u);
+
 plot(angles,Ajet,xaxis="θ",yaxis="<ζ>",color=:black,label="Jet");
-plot!(angles,A1[end,:],xaxis="θ",yaxis="<ζ>",linewidth=2,label="NL");
-plot!(angles,A2[end,:],xaxis="θ",yaxis="<ζ>",linewidth=2,label="GQL($Λ)");
-_zt = plot!(angles,A3[end,:],legend=:bottomright,xaxis="θ",yaxis="<ζ>",linewidth=2,label="GCE2($Λ)");
-savefig(_zt,dn*"zt_"*"$Λ"*".png");
+plot!(angles,A_nl[end,:],xaxis="θ",yaxis="<ζ>",label="NL");
+plot!(angles,A_ql[end,:],xaxis="θ",yaxis="<ζ>",label="QL");
+_zt = plot!(angles,A_gql[end,:],legend=:bottomright,xaxis="θ",yaxis="<ζ>",label="GQL($Λ)");
+_zt = plot!(angles,A_gce2[end,:],legend=:bottomright,xaxis="θ",yaxis="<ζ>",label="GCE2($Λ)");
+savefig(_zt,dn*"zt_2.png");
 
 ## Spatial vorticity
 xx = LinRange(-lx/2,lx/2,2*nx-1);
 yy = LinRange(-ly/2,ly/2,2*ny-1);
 
-U1 = inversefourier(nx,ny,sol1.u);
-_u = plot(xx,yy,U1[:,:,begin],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
-savefig(_u,dn*"NL_z_init.png");
-_u = plot(xx,yy,U1[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+U = inversefourier(nx,ny,sol_nl.u);
+_u = plot(xx,yy,U[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
 savefig(_u,dn*"NL_z_end.png");
 
-U2 = inversefourier(nx,ny,sol2.u);
-_u = plot(xx,yy,U2[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+U = inversefourier(nx,ny,sol_ql.u);
+_u = plot(xx,yy,U[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+savefig(_u,dn*"QL_z_end.png");
+
+U = inversefourier(nx,ny,sol_gql.u);
+_u = plot(xx,yy,U[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
 savefig(_u,dn*"GQL_"*"$Λ"*"_z_end.png");
 
-U3 = inversefourier(nx,ny,Λ,sol3.u);
-_u = plot(xx,yy,U3[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
+U = inversefourier(nx,ny,Λ,sol_gce2.u);
+_u = plot(xx,yy,U[:,:,end],st=:contourf,color=:bwr,xaxis="x",yaxis="y");
 savefig(_u,dn*"GCE2_"*"$Λ"*"_z_end.png");
--- solve.jl
+++ solve.jl
@@ -1,54 +1,26 @@
 using Revise
 using ZonalFlow
-using Plots
 using YAML,JLD2
-using FFTW
 
 using Logging: global_logger
 using TerminalLoggers: TerminalLogger
 global_logger(TerminalLogger())
 
 include("loadparams.jl")
-
 mkpath(dn);
 
 ζ0 = ic_pert_eqm(lx,ly,nx,ny,Ξ,jw=Δθ); # one ic for all
 
-@time sol1 = nl(lx,ly,nx,ny,Ξ,β,τ,jw=Δθ,ic=ζ0,dt=dt,t_end=t_end,savefreq=savefreq);
-@time sol2 = gql(lx,ly,nx,ny,Λ,Ξ,β,τ,jw=Δθ,dt=dt,ic=ζ0,t_end=t_end,savefreq=savefreq);
-@time sol3 = gce2(lx,ly,nx,ny,Λ,Ξ,β,τ,jw=Δθ,ic=ζ0,dt=dt,t_end=t_end,poscheck=false,savefreq=savefreq);
+@time sol_nl = nl(lx,ly,nx,ny,Ξ,β,τ,jw=Δθ,ic=ζ0,dt=dt,t_end=t_end,savefreq=savefreq);
+@save dn*"nl.jld2" sol_nl
 
-@save dn*"nl.jld2" sol1
-@save dn*"gql_$Λ.jld2" sol2
-@save dn*"gce2_$Λ.jld2" sol3
+@time sol_ql = gql(lx,ly,nx,ny,0,Ξ,β,τ,jw=Δθ,dt=dt,ic=ζ0,t_end=t_end,savefreq=savefreq);
+@save dn*"ql.jld2" sol_ql
 
-# pyplot();
+for Λ in [1,2,3,6]
+    @time sol_gql = gql(lx,ly,nx,ny,Λ,Ξ,β,τ,jw=Δθ,dt=dt,ic=ζ0,t_end=t_end,savefreq=savefreq);
+    @save dn*"gql_$Λ.jld2" sol_gql
 
-## Zonal energy
-zones = reshape(["$i" for i = 0:1:nx-1],1,nx);
-
-P1,O1 = zonalenergy(lx,ly,nx,ny,sol1.u);
-_p = plot(sol1.t,P1,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright,linewidth=2);
-savefig(_p,dn*"NL_em.png");
-
-P2,O2 = zonalenergy(lx,ly,nx,ny,sol2.u);
-_p = plot(sol2.t,P2,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright,linewidth=2);
-savefig(_p,dn*"GQL_"*"$Λ"*"_em.png");
-
-P3,O3 = zonalenergy(lx,ly,nx,ny,Λ,sol3.u);
-_p = plot(sol3.t,P3,xaxis=("Time"),yscale=:log10,yaxis=("Energy in Zonal Mode",(1e-9,1e3)),labels=zones,palette=:tab10,legend=:outerright,linewidth=2);
-savefig(_p,dn*"GCE2_"*"$Λ"*"_em.png");
-
-## Hövmoller data: mean vorticity
-angles = (180.0/ly)*LinRange(-ly/2,ly/2,2*ny-1);
-
-A1 = meanvorticity(lx,ly,nx,ny,sol1.u);
-A2 = meanvorticity(lx,ly,nx,ny,sol2.u);
-A3 = meanvorticity(lx,ly,nx,ny,Λ,sol3.u);
-
-Ajet = real(ifft(ifftshift(ic_eqm(lx,ly,nx,ny,Ξ,jw=Δθ)[:,1])));
-plot(angles,Ajet,xaxis="θ",yaxis="<ζ>",color=:black,label="Jet");
-plot!(angles,A1[end,:],xaxis="θ",yaxis="<ζ>",linewidth=2,label="NL");
-plot!(angles,A2[end,:],xaxis="θ",yaxis="<ζ>",linewidth=2,label="GQL($Λ)");
-_zt = plot!(angles,A3[end,:],legend=:bottomright,xaxis="θ",yaxis="<ζ>",linewidth=2,label="GCE2($Λ)");
-savefig(_zt,dn*"zt_"*"$Λ"*".png");
+    @time sol_gce2 = gce2(lx,ly,nx,ny,Λ,Ξ,β,τ,jw=Δθ,ic=ζ0,dt=dt,t_end=t_end,poscheck=false,savefreq=savefreq);
+    @save dn*"gce2_$Λ.jld2" sol_gce2
+end
