158aeffea26e5d181539626bc8cbfdd4a4c6f670
Girish Nivarti <nvgirishs@gmail.com>
2020-11-11 12:41:29 +0000

    Commentify build bash script

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# File to generate precompile statements for
 julia --startup-file=no --trace-compile=precompile.jl gen_precompile.jl
-julia --startup-file=no --output-o sys.o -J"/Applications/Julia-1.5.app/Contents/Resources/julia/lib/julia/sys.dylib" gen_sysimage.jl
-gcc -shared -o sys.dylib -Wl,-all_load sys.o -L"/Applications/Julia-1.5.app/Contents/Resources/julia/lib" -ljulia
+
+# Output of unsafe_string(Base.JLOptions().image_file) in julia
+jpath="/Applications/Julia-1.5.app/Contents/Resources/julia/lib/julia/sys.dylib"
+julia --startup-file=no --output-o sys.o -J$jpath gen_sysimage.jl
+
+# Output of abspath(Sys.BINDIR, Base.LIBDIR) in julia
+lpath="/Applications/Julia-1.5.app/Contents/Resources/julia/lib"
+gcc -shared -o sys.dylib -Wl,-all_load sys.o -L$lpath -ljulia
