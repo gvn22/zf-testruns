a555086388adf3a68a3ca77ccf972f8c86769ed7
Girish Nivarti <nvgirishs@gmail.com>
2020-11-12 19:19:09 +0000

    Higher level of optimisation

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ julia --startup-file=no --trace-compile=precompile.jl gen_precompile.jl
 
 # Output of unsafe_string(Base.JLOptions().image_file) in julia
 jpath="/Applications/Julia-1.5.app/Contents/Resources/julia/lib/julia/sys.dylib"
-julia --startup-file=no --output-o sys.o -J$jpath gen_sysimage.jl
+julia --startup-file=no -O3 --output-o sys.o -J$jpath gen_sysimage.jl
 
 # Output of abspath(Sys.BINDIR, Base.LIBDIR) in julia
 lpath="/Applications/Julia-1.5.app/Contents/Resources/julia/lib"
